613fe33c9998ea56d5bf5f0419b8cb26c7769945
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-01 09:39:35 +0300

    collection - arraylist

---
+++ src/main/java/com/zaurtregulov/collection/ArrayListEx1.java
@@ -0,0 +1,23 @@
+package com.zaurtregulov.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<String>();
+        arrayList1.add("Zaur");
+        arrayList1.add("Ivan");
+        arrayList1.add("Mariya");
+        System.out.println(arrayList1);
+
+        ArrayList<String> arrayList2 = new ArrayList<String>(200);
+        arrayList2.add("Zaur");
+        arrayList2.add("Ivan");
+        List<String> arrayList3 = new ArrayList<String>();
+
+        ArrayList<String> arrayList4 = new ArrayList<String>(arrayList1);
+        System.out.println(arrayList4);
+        System.out.println(arrayList1==arrayList4);
+    }
+}
