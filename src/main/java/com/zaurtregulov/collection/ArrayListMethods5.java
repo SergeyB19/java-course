4db4fb623789a60fadaa5ff6066598585d9d72f0
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-01 12:28:55 +0300

    методы Arraylist

---
+++ src/main/java/com/zaurtregulov/collection/ArrayListMethods5.java
@@ -0,0 +1,47 @@
+package com.zaurtregulov.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListMethods5 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<String>();
+        arrayList1.add("Zaur");
+        arrayList1.add("Ivan");
+        arrayList1.add("Mariya");
+        arrayList1.add("Kolya");
+        arrayList1.add("Elena");
+        List<Integer> list1 = List.of(3,8,13);
+        System.out.println("list1 = List.of = " + list1);
+        List<String> list2 = List.copyOf(arrayList1);
+        System.out.println("list2 = List.copyOf = " + list2);
+
+
+        System.out.println(arrayList1);
+
+        Object[] array = arrayList1.toArray();
+        String array2 = Arrays.toString(arrayList1.toArray(new String[0]));
+        System.out.println("array2 = " + array2);
+
+        List<String> myList = arrayList1.subList(1, 4);
+        System.out.println("myList = " + myList);
+        myList.add("Fedor");
+        System.out.println("myList = " + myList);
+        System.out.println("arrayList1 + myList = " + arrayList1);
+        arrayList1.add("Sveta");
+        System.out.println("arrayList1 = " + arrayList1);
+
+
+        ArrayList<String> arrayList2 = new ArrayList<String>();
+        arrayList2.add("Ivan");
+        arrayList2.add("Mariya");
+        arrayList2.add("Igor");
+
+        arrayList1.removeAll(arrayList2);
+        System.out.println("arrayList1 = " + arrayList1);
+
+        boolean result = arrayList1.containsAll(arrayList2);
+        System.out.println("result containsAll = " + result);
+    }
+}
