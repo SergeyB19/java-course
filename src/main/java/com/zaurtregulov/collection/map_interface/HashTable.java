245470398b402fee67dca06339e5a62fefd09bed
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-03 16:02:04 +0300

    HashTable

---
+++ src/main/java/com/zaurtregulov/collection/map_interface/HashTable.java
@@ -0,0 +1,17 @@
+package com.zaurtregulov.collection.map_interface;
+
+import java.util.Hashtable;
+public class HashTable {
+    public static void main(String[] args) {
+        Hashtable<Double, Student> ht = new Hashtable<>();
+        Student st1 = new Student("Zaur", "Tregulov", 3);
+        Student st2 = new Student("Mariya", "Ivanova", 1);
+        Student st3 = new Student("Sergey", "Petrov", 4);
+        Student st4 = new Student("Igor", "Sidorov", 2);
+        ht.put(7.8, st1);
+        ht.put(9.3, st2);
+        ht.put(5.8, st3);
+        ht.put(6.0, st4);
+        System.out.println("ht = " + ht);
+    }
+}
