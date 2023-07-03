e436b725817de6af0b686f21705700355496dfce
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-03 15:55:24 +0300

    LinkedHashMap

---
+++ src/main/java/com/zaurtregulov/collection/map_interface/LinkedHashMapExample.java
@@ -0,0 +1,25 @@
+package com.zaurtregulov.collection.map_interface;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class LinkedHashMapExample {
+    public static void main(String[] args) {
+        LinkedHashMap<Double, Student> lhm = new LinkedHashMap<>(16,0.75f,true);
+        Student st1 = new Student("Zaur", "Tregulov", 3);
+        Student st2 = new Student("Mariya", "Ivanova", 1);
+        Student st3 = new Student("Sergey", "Petrov", 4);
+        Student st4 = new Student("Igor", "Sidorov", 2);
+
+        lhm.put(7.2, st3);
+        lhm.put(7.5, st4);
+        lhm.put(5.8, st1);
+        lhm.put(6.4, st2);
+        System.out.println("lhm = " + lhm);
+        System.out.println("lhm.get() = " + lhm.get(6.4));
+        System.out.println("lhm.get() = " + lhm.get(7.5));
+        System.out.println("lhm.get() = " + lhm.get(7.2));
+        System.out.println("lhm.get() = " + lhm.get(5.8));
+        System.out.println("lhm = " + lhm);
+
+    }
+}
