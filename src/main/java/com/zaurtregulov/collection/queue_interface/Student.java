16ee00f265b8508eaaba8aa20b7272ac4b3880d8
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-04 10:00:21 +0300

    PriorityQueue

---
+++ src/main/java/com/zaurtregulov/collection/queue_interface/Student.java
@@ -0,0 +1,39 @@
+package com.zaurtregulov.collection.queue_interface;
+
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
+    String name;
+    int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.course-other.course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course);
+    }
+}
