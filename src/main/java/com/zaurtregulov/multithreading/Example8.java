5ea6f00397eac22bdab9ad4e45b40ba9c88f6056
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-05 20:30:29 +0300

    Thread sleep && join

---
+++ src/main/java/com/zaurtregulov/multithreading/Example8.java
@@ -0,0 +1,40 @@
+package com.zaurtregulov.multithreading;
+
+public class Example8 extends Thread{
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new MyRunnable1());
+        Example8 thread2 = new Example8();
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        System.out.println("Konec!");
+    }
+}
+
+class MyRunnable1 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
