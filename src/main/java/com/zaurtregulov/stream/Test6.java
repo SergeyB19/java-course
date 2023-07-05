b4202250a92f50704cf2cffbe95cb6d3ee707bd1
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-05 13:17:35 +0300

    Streams -> distinct

--- src/main/java/com/zaurtregulov/stream/Test6.java
+++ src/main/java/com/zaurtregulov/stream/Test6.java
@@ -15,6 +15,11 @@ public class Test6 {
         Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
         Stream<Integer> stream4 = Stream.concat(stream2, stream3);
         stream4.forEach(System.out::println);
+        System.out.println("----------------------------------");
+        Stream<Integer> stream5 = Stream.of(1, 2, 3, 4, 5,1,2,3);
+        stream5.distinct().forEach(System.out::println);
+
+
 
     }
 }
