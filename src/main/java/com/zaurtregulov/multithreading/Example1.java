12359776ccb49ab3d98f8dad3f0ac4e14ebc0e57
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-05 19:49:32 +0300

    многопоточность

---
+++ src/main/java/com/zaurtregulov/multithreading/Example1.java
@@ -0,0 +1,11 @@
+package com.zaurtregulov.multithreading;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("Privet");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("poka");
+    }
+}
