4ea92a76daca51342acd0da277c6843f3e8098fc
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-06 10:12:09 +0300

    volatile

--- src/main/java/com/zaurtregulov/multithreading/Example8.java
+++ src/main/java/com/zaurtregulov/multithreading/Example8.java
@@ -4,7 +4,7 @@ public class Example8 extends Thread{
     public void run() {
         for (int i = 1; i <= 10; i++) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -30,7 +30,7 @@ class MyRunnable1 implements Runnable{
     public void run() {
         for (int i = 1; i <= 10; i++) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
