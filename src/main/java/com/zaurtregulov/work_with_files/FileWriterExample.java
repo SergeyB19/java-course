8ba9b499cfc02eb8da96cc83e84fed41cb625c58
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-07 13:43:52 +0300

    FileWriter

---
+++ src/main/java/com/zaurtregulov/work_with_files/FileWriterExample.java
@@ -0,0 +1,31 @@
+package com.zaurtregulov.work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterExample {
+    public static void main(String[] args) throws IOException {
+        String rubai = "Много лет размышлял я над жизнью земной. \n" +
+                "Непонятного нет для меня под луной. \n" +
+                "Мне известно, что мне ничего не известно! \n" +
+                "Вот последняя правда открытая мной.\n";
+        String s = "privet";
+        FileWriter writer = null;
+        try {
+//            writer = new FileWriter("C:\\Users\\Пользователь\\Desktop\\test1.txt");
+            writer = new FileWriter("test2.txt",true);
+
+//            for (int i = 0; i < rubai.length(); i++) {
+//                writer.write(rubai.charAt(i));
+//            }
+//            writer.write(rubai);
+            writer.write(s);
+            System.out.println("Done!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            writer.close();
+        }
+    }
+}
