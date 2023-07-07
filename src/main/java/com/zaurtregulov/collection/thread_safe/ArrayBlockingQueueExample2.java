68e8b696ca4f599c970a38b07445d24c9b14aecb
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-07 13:22:35 +0300

    ArrayBlockingQueue

---
+++ src/main/java/com/zaurtregulov/collection/thread_safe/ArrayBlockingQueueExample2.java
@@ -0,0 +1,36 @@
+package com.zaurtregulov.collection.thread_safe;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ArrayBlockingQueueExample2 {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(4);
+
+        //Producer
+        new Thread(()->{
+            int i = 0;
+            while (true) {
+                try {
+                    arrayBlockingQueue.put(++i);
+                    System.out.println("Producer dobavil: " + i + " " + arrayBlockingQueue);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        //Consumer
+        new Thread(()->{
+            while (true) {
+                try {
+                    Integer j = arrayBlockingQueue.take();
+                    System.out.println("Consumer zabral: " + j + " " + arrayBlockingQueue);
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+}
