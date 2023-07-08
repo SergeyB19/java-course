efe644a0e31b9e10616bb2702c6953b7a4e7d865
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-08 10:49:22 +0300

    FileInputStream && FileOutputStream

--- src/main/java/com/zaurtregulov/work_with_files/CopyExample.java
+++ src/main/java/com/zaurtregulov/work_with_files/CopyExample.java
@@ -4,18 +4,18 @@ import java.io.*;
 
 public class CopyExample {
     public static void main(String[] args) {
-        try(BufferedReader reader = new BufferedReader(new FileReader("test2.txt"));
-            BufferedWriter writer = new BufferedWriter(new FileWriter("test3.txt"));
+        try(BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Пользователь\\Desktop\\IMG_20220716_112846.jpg"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("IMG_20220716_112846.jpg"));
         ) {
-//            int character;
-//            while ((character = reader.read()) != -1) {
-//                writer.write(character);
-//            }
-            String line;
-            while ((line = reader.readLine()) != null) {
-                writer.write(line);
-                writer.write('\n');
+            int character;
+            while ((character = reader.read()) != -1) {
+                writer.write(character);
             }
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                writer.write(line);
+//                writer.write('\n');
+//            }
             System.out.println("Done");
 
         } catch (FileNotFoundException e) {
