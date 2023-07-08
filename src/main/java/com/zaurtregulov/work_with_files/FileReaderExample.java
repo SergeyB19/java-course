a11292285b55418a6838136d9810c5555099e86b
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-08 10:22:59 +0300

    Try with resources

--- src/main/java/com/zaurtregulov/work_with_files/FileReaderExample.java
+++ src/main/java/com/zaurtregulov/work_with_files/FileReaderExample.java
@@ -2,13 +2,15 @@ package com.zaurtregulov.work_with_files;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileReaderExample {
-    public static void main(String[] args) {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("test2.txt");
+    public static void main(String[] args) throws IOException {
+//        FileReader reader = null;
+        try (FileReader reader = new FileReader("test2.txt");
+             FileWriter writer = new FileWriter("test2.txt",true);) {
+//            reader = new FileReader("test2.txt");
             int character;
             while ((character = reader.read()) != -1) {
                 System.out.print((char) character);
@@ -17,15 +19,13 @@ public class FileReaderExample {
             System.out.println("Done");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-        finally {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+//        finally {
+//            try {
+//                reader.close();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
     }
 }
--- src/main/java/com/zaurtregulov/work_with_files/FileWriterExample.java
+++ src/main/java/com/zaurtregulov/work_with_files/FileWriterExample.java
@@ -10,22 +10,17 @@ public class FileWriterExample {
                 "Мне известно, что мне ничего не известно! \n" +
                 "Вот последняя правда открытая мной.\n";
         String s = "privet";
-        FileWriter writer = null;
-        try {
-//            writer = new FileWriter("C:\\Users\\Пользователь\\Desktop\\test1.txt");
-            writer = new FileWriter("test2.txt",true);
 
-//            for (int i = 0; i < rubai.length(); i++) {
-//                writer.write(rubai.charAt(i));
-//            }
+        try( FileWriter writer = new FileWriter("test2.txt",true);) {
+//            writer = new FileWriter("C:\\Users\\Пользователь\\Desktop\\test1.txt");
+            for (int i = 0; i < rubai.length(); i++) {
+                writer.write(rubai.charAt(i));
+            }
 //            writer.write(rubai);
             writer.write(s);
             System.out.println("Done!");
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-        finally {
-            writer.close();
         }
     }
 }
