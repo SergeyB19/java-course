0ca2d45fb0094d4673229a5d817a1be737b5c88e
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-09 12:11:20 +0300

    RegEx part#2

--- src/main/java/com/zaurtregulov/regex/Regex2.java
+++ src/main/java/com/zaurtregulov/regex/Regex2.java
@@ -15,8 +15,34 @@ public class Regex2 {
 //        Pattern pattern1 = Pattern.compile("abc[^e-g4-7]");
 
 
-        String s1 = "abcd abce abc5abcg6abch";
-        Pattern pattern1 = Pattern.compile("abc(e|5)");
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc(e|5)");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc.");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc5abcg6abch$");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("[0-9]");
+//
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\d");
+//
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\D");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\w");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\W");
+
+        String s1 = "abcd!?abce====abc ++5abcg6abch";
+        Pattern pattern1 = Pattern.compile("\\w{4}");
+
+
 
         Matcher matcher = pattern1.matcher(s1);
 
